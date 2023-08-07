1bb3b051dcfd24a7ca82f70a6cd01b8de053a7c1
Manoj-185 <81622199+Manoj-185@users.noreply.github.com>
2023-08-07 11:06:16 +0530

    add file rate limit

---
+++ lib/custom_rate_limit.rb
@@ -0,0 +1,29 @@
+class CustomRateLimit
+  LIMIT = 20 
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if should_allow?(env)
+      @app.call(env)
+    else
+      request_quota_exceeded
+    end
+  end
+
+  private
+  def should_allow?(env)
+    return true if env['rack.session']['warden.user.user.key'].present?
+
+    key = "IP1:#{env['action_dispatch.remote_ip']}"
+    
+     REDIS.set(key, 0, nx: true, ex: 60.seconds)
+     REDIS.incr(key) > LIMIT ? false : true
+  end
+
+  def request_quota_exceeded
+    [ 429, {}, ['Too many requests fired. Request quota exceeded!'] ]
+  end
+end
